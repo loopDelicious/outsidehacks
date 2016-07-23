2dfd15be65055a275c8ee122977c6494347ab108
Joyce Lin <joycelin79@gmail.com>
2016-07-23 16:08:13 -0700

    web sockets with admin color changer working

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.send('<h1>Hello Joyce</h1>');
+    res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', function(socket){
+  setTimeout(function(){
+    socket.emit("background color", "blue");
+  },1000);
+
+
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log(msg)
+  });
+
+
+  socket.on('admin-color', function(color){
+    io.emit("background color", color);
+  })
+});
+
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
+
+
