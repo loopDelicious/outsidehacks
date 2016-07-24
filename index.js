c8742c10908dced3db149681654544f0f6b118f9
Joyce Lin <joycelin79@gmail.com>
2016-07-23 18:30:15 -0700

    check client ids and update locations

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var clients = {}; // 
+//socket id: [location, color, last message / pic
+//find id on location update
 
 app.use(express.static('static'));
 
@@ -9,20 +12,39 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
+io.on('connection', function (client) {
     console.log('a user connected');
-    socket.on('disconnect', function () {
+    client.on('disconnect', function () {
         console.log('user disconnected');
     });
 
-
+    // update location
+    client.on('location-update', function(latlng){
+        // if user in clients, push location
+        // otherwise create new client and push location
+        if (typeof(clients[client.id]) == "object") {
+            client[client.id].coords = latlng;
+        } else {
+            clients[client.id] = {
+                'coords': latlng,
+                'first_appeared': Date.now(),
+            }
+        }
+    });
     // display logic:
 
-    socket.on('admin-color', function (color) {
+    client.on('admin-color', function (color) {
         io.emit("background color", color);
     });
 
+    client.on('admin-listclients', function() {
+        client.emit("clients", clients);
+    });
+
 });
+
+
+
 
 http.listen(3000, function () {
     console.log('listening on *:3000');
